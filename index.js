b449f228ab18b2e443922500f724d94243fed7db
NIKHILA REDDY <iamnikhilareddy@gmail.com>
2025-08-14 16:00:28 +0530

    passing data to EJS

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ app.set("views",path.join(__dirname,"/views"));
 app.listen(port,()=>{
     console.log(`listening on port ${port}`);
 })
-app.get("/",(req,res)=>{
-    res.render("home.ejs");
+app.get("/rolldice",(req,res)=>{
+    let diceVal=Math.floor(Math.random()*6)+1;
+    res.render("home.ejs",{diceVal});
 })
