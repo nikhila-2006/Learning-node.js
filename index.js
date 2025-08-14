fb22316b844a2ff1675b656b0bd5acc4f6ff8d11
NIKHILA REDDY <iamnikhilareddy@gmail.com>
2025-08-14 13:21:51 +0530

    access views

---
+++ index.js
@@ -0,0 +1,15 @@
+const express=require("express");
+const app=express();
+let port=8080;
+let path=require("path");
+
+
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"/views"));
+
+app.listen(port,()=>{
+    console.log(`listening on port ${port}`);
+})
+app.get("/",(req,res)=>{
+    res.render("home.ejs");
+})
