4b8c432443a1b23bf0ea0bb00ac139da9f4e0c4e
NIKHILA REDDY <iamnikhilareddy@gmail.com>
2025-08-15 13:10:28 +0530

    Handling post requests

--- Backend/index.js
+++ Backend/index.js
@@ -1,12 +1,15 @@
 const express=require("express");
 const app=express();
 let port=8080;
+app.use(express.urlencoded({expected:true}));
+app.use(express.json());
 app.get("/registry",(req,res)=>{
     let {username,password}=req.query;
     res.send(`Form Submitted by GET request, Welcome <b> ${username} </b>`);
 })
 app.post("/registry",(req,res)=>{
-    res.send("<h1>Form Submitted by POST request</h1>");
+    let {username,password}=req.body;
+    res.send(`Form Submitted by POST request, Welcome <b> ${username} </b>`);
 })
 app.listen(port,()=>{
     console.log(`listening on port ${port}`);
