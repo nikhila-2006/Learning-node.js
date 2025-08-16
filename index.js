380d28cbeee9ca3a1f191c9a9e3c9e198ac0f876
NIKHILA REDDY <iamnikhilareddy@gmail.com>
2025-08-16 17:29:16 +0530

    res.redirect(url)

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.get("/posts/new",(req,res)=>{
 app.post("/posts",(req,res)=>{
     let {username,content}=req.body;
     posts.push({username,content});
-    res.send("Serving your new post");
+    res.redirect("/posts");
 })
 app.listen(port,()=>{
     console.log("listening to the port 8080");
