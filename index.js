abd689b38eeb0e358be34933dcb24bcea14ac645
NIKHILA REDDY <iamnikhilareddy@gmail.com>
2025-08-16 19:31:03 +0530

    Creat unique id's

--- index.js
+++ index.js
@@ -2,13 +2,17 @@ const express=require("express");
 const app=express();
 const port=8080;
 const path=require("path");
+const { v4: uuidv4 } = require('uuid');
 let posts=[{
+    id:uuidv4(),
     username:"NikhilaReddy",
     content:"I am selected for my 1st internship at microsoft"
 },{
+    id:uuidv4(),
     username:"Shradhakhapra",
     content:"Smile looks good on you :)"
 },{
+    id:uuidv4(),
     username:"Apnacollge",
     content:"coding is fun!"
 }];
@@ -24,8 +28,14 @@ app.get("/posts/new",(req,res)=>{
 })
 app.post("/posts",(req,res)=>{
     let {username,content}=req.body;
-    posts.push({username,content});
+    let id=uuidv4();
+    posts.push({id,username,content});
     res.redirect("/posts");
+})
+app.get("/posts/:id",(req,res)=>{
+    let {id}=req.params;
+    let post=posts.find((p)=> id === p.id);
+    res.render("show.ejs",{post});
 })
 app.listen(port,()=>{
     console.log("listening to the port 8080");
